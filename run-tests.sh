7ea7a548528bac2788dc4b4ee3b85f2410ba2216
Thomas von Dein <tom@izb.net>
2017-07-18 19:45:02 +0200

    added

---
+++ run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+if [ -n "$TRAVIS" ]; then
+    cd "$(dirname "$0")"
+
+    ECUKES_EMACS=${EMACS:-$(which emacs)}
+    export ECUKES_EMACS
+
+    echo "*** Emacs version ***"
+    echo "ECUKES_EMACS = $ECUKES_EMACS"
+    "$ECUKES_EMACS" --version
+    echo
+fi
+
+cask emacs --batch -Q -l package-lint.el -f package-lint-batch-and-exit autoscratch-mode.el
+cask exec ert-runner -L . -L test "$@"
