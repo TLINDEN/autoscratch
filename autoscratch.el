f8f2a66bc933103199c5b9fb03f35c83e2e0ee17
Thomas von Dein <tom@vondein.org>
2023-06-01 19:44:39 +0200

    checkdoc

--- autoscratch.el
+++ autoscratch.el
@@ -28,7 +28,7 @@
 ;;; Commentary:
 ;;;; Introduction:
 ;; This simple  major mode can be  used as the initial  major mode for
-;; the scratch buffer. It automatically switches to another major mode
+;; the scratch buffer.  It automatically switches to another major mode
 ;; based on regexes triggered by text input.
 ;;
 ;; In   the   default  configuration   it   responds   to  the   first
@@ -49,7 +49,7 @@
 ;; You     may,     however,     configure    the     trigger     list
 ;; `autoscratch-triggers-alist' according  to your  preferences.  This
 ;; list consists  of cons cells, where  the `car' is a  regexp and the
-;; `cdr' an emacs lisp form (e.g. a  lambda or defun).  If you want to
+;; `cdr' an Emacs Lisp form (e.g. a  lambda or defun).  If you want to
 ;; use regexps which  match more than one character, then  you need to
 ;; set `autoscratch-trigger-on-first-char' to  `nil' and possibly tune
 ;; `autoscratch-trigger-after' accordingly.
@@ -61,7 +61,7 @@
 
 ;; Autoscratch can  also be  configured to  rename the  current buffer
 ;; after  it  switched mode  based  on  a  trigger  and create  a  new
-;; `autoscratch-buffer'  in the  background. In  order to  enable this
+;; `autoscratch-buffer'  in the  background.  In  order to  enable this
 ;; feature, set `autoscratch-fork-after-trigger' to t.
 
 ;; To   further    tune   the   trigger   bahavior    you   can   tune
